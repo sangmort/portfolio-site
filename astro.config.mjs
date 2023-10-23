da9cec8fad83daf0a2be1ec1f41b2d8c69bc594a
sangmort <22422853+sangmort@users.noreply.github.com>
2023-10-23 19:05:49 -0400

    Deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-// export default defineConfig({
-//   site: "osyassen.com",
-// });
+//astro.build/config
+export default defineConfig({
+  site: "https://sangmort.github.io/portfolio-site/",
+  base: `/portoflio-site`,
+});
