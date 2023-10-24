2428b80ada481cc653b5d9f3d3065e3b9378d5d1
sangmort <22422853+sangmort@users.noreply.github.com>
2023-10-23 20:26:24 -0400

    Fix typo in base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 //astro.build/config
 export default defineConfig({
   site: "https://sangmort.github.io/",
-  base: `/portoflio-site`,
+  base: `/portfolio-site`,
 });
