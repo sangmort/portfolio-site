796cde4945b5943f914cc8a4a455b24129a529ab
sangmort <22422853+sangmort@users.noreply.github.com>
2023-10-26 11:28:16 -0400

    Update link paths to work on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 //astro.build/config
 export default defineConfig({
-  site: "https://sangmort.github.io/portfolio-site/"
-//   base: "https://sangmort.github.io/portfolio-site",
+  site: "https://sangmort.github.io",
+  base: "/portfolio-site",
 });
