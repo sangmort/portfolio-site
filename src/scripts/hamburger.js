7dba01c307155580371bb7b445db82d046eab067
sangmort <22422853+sangmort@users.noreply.github.com>
2023-10-27 15:43:08 -0400

    Rename .header-links to .navbar-links for clarity

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -1,7 +1,7 @@
 document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".header-links").classList.toggle("expanded");
+  document.querySelector(".navbar-links").classList.toggle("expanded");
 });
 
 document.querySelector(".close-navigation").addEventListener("click", () => {
-  document.querySelector(".header-links").classList.toggle("expanded");
+  document.querySelector(".navbar-links").classList.toggle("expanded");
 });
