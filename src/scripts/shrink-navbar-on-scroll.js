c073bc82f33672b44f2a4f96416f9366f06bb54e
sangmort <22422853+sangmort@users.noreply.github.com>
2023-10-26 21:54:45 -0400

    Refactor nav bar to shrink on-scroll & sticky

---
+++ src/scripts/shrink-navbar-on-scroll.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+  const navbar = document.querySelector(".navbar-default");
+  const scrollThreshold = 100;
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > scrollThreshold) {
+      navbar.classList.add("navbar-shrink");
+      header.style.position = "sticky";
+      header.style.top = "0";
+    } else {
+      navbar.classList.remove("navbar-shrink");
+      header.style.position = "relative";
+      header.style.top = "initial";
+    }
+  });
+});
