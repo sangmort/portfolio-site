aea377a8dc6c73a720562470f3732789dd16ae79
sangmort <22422853+sangmort@users.noreply.github.com>
2023-10-28 18:52:03 -0400

    Add third cloud design to animation

--- src/scripts/modify-clouds.js
+++ src/scripts/modify-clouds.js
@@ -1,11 +1,22 @@
-const numCopies = 30;
+const numCopies = 60;
 const container = document.getElementById("cloud-container");
 const originalCloud1 = document.querySelector(".cloud1").cloneNode(true);
 const originalCloud2 = document.querySelector(".cloud2").cloneNode(true);
+const originalCloud3 = document.querySelector(".cloud3").cloneNode(true);
 
 const duplicateClouds = () => {
   for (let i = 0; i < numCopies; i++) {
-    const clonedCloud = (i % 2 === 0) ? originalCloud1.cloneNode(true) : originalCloud2.cloneNode(true);
+    let clonedCloud;
+    const randomType = Math.floor(Math.random() * 3); // Generates 0, 1, or 2
+
+    if (randomType === 0) {
+      clonedCloud = originalCloud1.cloneNode(true);
+    } else if (randomType === 1) {
+      clonedCloud = originalCloud2.cloneNode(true);
+    } else {
+      clonedCloud = originalCloud3.cloneNode(true);
+    }
+
     clonedCloud.style.position = "absolute";
 
     const randomX = Math.random() * (container.clientWidth - clonedCloud.clientWidth);
