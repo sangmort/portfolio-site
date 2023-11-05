e8296c6780dbf6a6cd8f00eb2a465c2804055db2
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-04 21:57:01 -0400

    Increase scroll threshold for better UX

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -14,3 +14,34 @@ document.addEventListener("click", (event) => {
     navigation.classList.remove("expanded");
   }
 });
+
+
+window.addEventListener('mousemove', function (e) {
+  const sizes = [1, 0.9, 0.8, 0.75, 0.6];
+
+  sizes.forEach((scale) => {
+    const randomOffset = (1 - scale) * 200;
+    const elem = document.createElement("div");
+    const size =  3 * scale + 'px';
+
+    elem.style.position = 'fixed';
+    elem.style.top = e.pageY + Math.floor(Math.random() * randomOffset - randomOffset / 2) + 'px';
+    elem.style.left = e.pageX + Math.floor(Math.random() * randomOffset - randomOffset / 2) + 'px';
+    elem.style.width = size;
+    elem.style.height = size;
+    elem.style.background = `hsla(${Math.round(Math.random() * 0)}, 100%, 90%, ${scale})`;
+    elem.style.borderRadius = size;
+    elem.style.opacity = '.75';
+    elem.style.filter = 'drop-shadow(0 0 2px #fff)'
+    elem.style.pointerEvents = 'none';
+
+    // Set the z-index to 1200
+    elem.style.zIndex = 1200;
+
+    document.body.appendChild(elem);
+
+    window.setTimeout(function () {
+      document.body.removeChild(elem);
+    }, Math.round(Math.random() * scale * 500));
+  });
+}, false);
