fcaff7d3507545fb72336d75555b94f2f5970823
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-07 12:39:07 -0500

    Refactor to stop page jumping & add comments

--- src/scripts/shrink-navbar-on-scroll.js
+++ src/scripts/shrink-navbar-on-scroll.js
@@ -1,21 +1,43 @@
+// Shrink navbar on scroll & makes it sticky
 document.addEventListener("DOMContentLoaded", function () {
   const headerLogo = document.getElementById("header-logo");
   const header = document.querySelector("header");
   const navbar = document.querySelector(".navbar-default");
-  const scrollThreshold = 600;
+  const scrollThreshold = 300;
 
+  // Track whether navigation is sticky
+  let isNavigationSticky = false;
+
+  // Maker Header sticky on scroll
   window.addEventListener("scroll", function () {
-    if (window.scrollY > scrollThreshold) {
+    if (window.scrollY > scrollThreshold && !isNavigationSticky) {
       navbar.classList.add("navbar-shrink");
       header.style.position = "sticky";
       header.style.top = "0";
       headerLogo.style.display = "none";
+
+      // Create a placeholder when navigation is sticky to stop page from jumping
+      const headerPlaceholder = document.createElement("div");
+      headerPlaceholder.style.height = header.clientHeight + "px";
+      header.insertBefore(headerPlaceholder, header.firstChild);
+
+      isNavigationSticky = true;
     }
-    if (window.scrollY < scrollThreshold) {
+
+    // Reset navigation when user scrolls up to be no longer sticky
+    if (window.scrollY < scrollThreshold && isNavigationSticky) {
       navbar.classList.remove("navbar-shrink");
       header.style.position = "relative";
       header.style.top = "initial";
       headerLogo.style.display = "block";
+
+      // Remove placeholder when navigation is no longer sticky
+      const headerPlaceholder = document.querySelector("#header-placeholder");
+      if (headerPlaceholder) {
+        headerPlaceholder.remove();
+      }
+
+      isNavigationSticky = false;
     }
   });
 });
