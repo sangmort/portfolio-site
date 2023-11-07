65503eecd944e0dfa7b01455a1e849a3e62354b2
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-06 20:23:10 -0500

    Refactor fireflies

--- src/scripts/hamburger.js
+++ src/scripts/hamburger.js
@@ -15,33 +15,51 @@ document.addEventListener("click", (event) => {
   }
 });
 
+// Fireflies that follow user's mouse cursor
+window.addEventListener(
+  "mousemove",
+  function (e) {
+    const numberOfFireflies = 20;
+    const colors = ["var(--fireflies-color-1)", "var(--fireflies-color-2)", "var(--fireflies-color-3)"];
 
-window.addEventListener('mousemove', function (e) {
-  const sizes = [1, 0.9, 0.8, 0.75, 0.6];
+    function createFirefly() {
+      const scale = 0.6 + Math.random() * 0.7;
+      const randomOffset = (1 - scale) * 300;
+      const size = 5 * scale + "px";
 
-  sizes.forEach((scale) => {
-    const randomOffset = (1 - scale) * 200;
-    const elem = document.createElement("div");
-    const size =  3 * scale + 'px';
+      const elem = document.createElement("div");
+      elem.style.position = "fixed";
+      elem.style.top = e.clientY + Math.floor(Math.random() * randomOffset - randomOffset / 2) + "px";
+      elem.style.left = e.clientX + Math.floor(Math.random() * randomOffset - randomOffset / 2) + "px";
+      elem.style.width = size;
+      elem.style.height = size;
+      elem.style.background = colors[Math.floor(Math.random() * colors.length)];
+      elem.style.borderRadius = size;
+      elem.style.filter = `drop-shadow(0 0 .2rem ${colors[Math.floor(Math.random() * colors.length)]}) blur(.04rem)`;
+      elem.style.pointerEvents = "none";
+      elem.style.zIndex = 1200;
 
-    elem.style.position = 'fixed';
-    elem.style.top = e.pageY + Math.floor(Math.random() * randomOffset - randomOffset / 2) + 'px';
-    elem.style.left = e.pageX + Math.floor(Math.random() * randomOffset - randomOffset / 2) + 'px';
-    elem.style.width = size;
-    elem.style.height = size;
-    elem.style.background = `hsla(${Math.round(Math.random() * 0)}, 100%, 90%, ${scale})`;
-    elem.style.borderRadius = size;
-    elem.style.opacity = '.75';
-    elem.style.filter = 'drop-shadow(0 0 2px #fff)'
-    elem.style.pointerEvents = 'none';
+      // Random opacity and animation duration
+      elem.style.opacity = Math.random() * 0.6 + 0.2;
+      elem.style.transition = "opacity " + (Math.random() * 3 + 1) + "s ease";
 
-    // Set the z-index to 1200
-    elem.style.zIndex = 1200;
+      document.body.appendChild(elem);
 
-    document.body.appendChild(elem);
+      // Remove fireflies
+      window.setTimeout(function () {
+        document.body.removeChild(elem);
+      }, Math.round(Math.random() * scale * 360));
 
-    window.setTimeout(function () {
-      document.body.removeChild(elem);
-    }, Math.round(Math.random() * scale * 500));
-  });
-}, false);
+      // Random blinking
+      setInterval(function () {
+        elem.style.opacity = Math.random() * 0.6 + 0.2;
+      }, Math.random() * 2000 + 100);
+    }
+
+    // Create fireflies
+    for (let i = 0; i < numberOfFireflies; i++) {
+      createFirefly();
+    }
+  },
+  false
+);
