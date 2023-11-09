081b8a1f127342e2717f0e7d4dd3abe1971caa30
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-09 18:17:42 -0500

    Tweak scroll threshold

--- src/scripts/shrink-navbar-on-scroll.js
+++ src/scripts/shrink-navbar-on-scroll.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const headerLogo = document.getElementById("header-logo");
   const header = document.querySelector("header");
   const navbar = document.querySelector(".navbar-default");
-  const scrollThreshold = 100;
+  const scrollThreshold = 200;
 
   // Track whether navigation is sticky
   let isNavigationSticky = false;
