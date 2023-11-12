c9574f08fad4ce48b558d0bccfe779312c51be97
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-11 20:01:31 -0500

    Refactor to account for initial navbar visibility

--- src/scripts/shrink-navbar-on-scroll.js
+++ src/scripts/shrink-navbar-on-scroll.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const navbar = document.querySelector(".navbar-links");
   const stickyNavbar = document.getElementById("sticky");
   const scrollThreshold = 350;
 
   window.addEventListener("scroll", function () {
     if (window.scrollY > scrollThreshold) {
+      navbar.style.visibility = "hidden";
       stickyNavbar.style.visibility = "visible";
       stickyNavbar.style.position = "fixed";
       stickyNavbar.style.top = "0";
     } else if (window.scrollY < scrollThreshold) {
       stickyNavbar.style.visibility = "hidden";
+      navbar.style.visibility = "visible";
     }
   });
 });
