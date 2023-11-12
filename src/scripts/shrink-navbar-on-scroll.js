ea194244b4f0730108a04f2cf909734bff0093be
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-11 19:43:44 -0500

    Decouple sticky navbar from header component

--- src/scripts/shrink-navbar-on-scroll.js
+++ src/scripts/shrink-navbar-on-scroll.js
@@ -1,43 +1,46 @@
 // Shrink navbar on scroll & makes it sticky
 document.addEventListener("DOMContentLoaded", function () {
-  const headerLogo = document.getElementById("header-logo");
-  const header = document.querySelector("header");
-  const navbar = document.querySelector(".navbar-default");
-  const scrollThreshold = 200;
+  // const headerLogo = document.getElementById("header-logo");
+  const stickyNavbar = document.getElementById("sticky");
+  // const header = document.querySelector("header");
+  // const navbar = document.querySelector(".navbar-links");
+  const scrollThreshold = 400;
 
   // Track whether navigation is sticky
-  let isNavigationSticky = false;
+  // let isNavigationSticky = false;
 
   // Maker Header sticky on scroll
   window.addEventListener("scroll", function () {
-    if (window.scrollY > scrollThreshold && !isNavigationSticky) {
-      navbar.classList.add("navbar-shrink");
-      header.style.position = "sticky";
-      header.style.top = "0";
-      headerLogo.style.visibility = "hidden";
+    if (window.scrollY > scrollThreshold ) {
+      // navbar.classList.add("navbar-shrink");
+      stickyNavbar.style.visibility = "visible";
+      stickyNavbar.style.position = "fixed";
+      stickyNavbar.style.top = "0";
+      // header.style.top = "0";
+      // headerLogo.style.display = "none";
 
       // Create a placeholder when navigation is sticky to stop page from jumping
-      const headerPlaceholder = document.createElement("div");
-      headerPlaceholder.style.height = header.clientHeight + "px";
-      header.insertBefore(headerPlaceholder, header.firstChild);
+      // const headerPlaceholder = document.createElement("div");
+      // headerPlaceholder.style.height = header.clientHeight + "px";
+      // header.insertBefore(headerPlaceholder, header.firstChild);
 
-      isNavigationSticky = true;
+      // isNavigationSticky = true;
     }
 
     // Reset navigation when user scrolls up to be no longer sticky
-    if (window.scrollY < scrollThreshold && isNavigationSticky) {
-      navbar.classList.remove("navbar-shrink");
-      header.style.position = "relative";
-      header.style.top = "25dvh";
-      headerLogo.style.visibility = "visible";
+    else if (window.scrollY < scrollThreshold ) {
+      // navbar.classList.remove("navbar-shrink");
+      stickyNavbar.style.visibility = "hidden";
+
+      // headerLogo.style.display = "flex";
 
       // Remove placeholder when navigation is no longer sticky
-      const headerPlaceholder = document.querySelector("#header-placeholder");
-      if (headerPlaceholder) {
-        headerPlaceholder.remove();
-      }
+      // const headerPlaceholder = document.querySelector("#header-placeholder");
+      // if (headerPlaceholder) {
+      //   headerPlaceholder.remove();
+      // }
 
-      isNavigationSticky = false;
+      // isNavigationSticky = false;
     }
   });
 });
