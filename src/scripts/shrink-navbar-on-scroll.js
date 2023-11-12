524ecf60e74213ed6b8c04c56b9d407019061bfa
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-11 19:49:17 -0500

    Remove unnecessary code

--- src/scripts/shrink-navbar-on-scroll.js
+++ src/scripts/shrink-navbar-on-scroll.js
@@ -1,46 +1,14 @@
-// Shrink navbar on scroll & makes it sticky
 document.addEventListener("DOMContentLoaded", function () {
-  // const headerLogo = document.getElementById("header-logo");
   const stickyNavbar = document.getElementById("sticky");
-  // const header = document.querySelector("header");
-  // const navbar = document.querySelector(".navbar-links");
-  const scrollThreshold = 400;
+  const scrollThreshold = 350;
 
-  // Track whether navigation is sticky
-  // let isNavigationSticky = false;
-
-  // Maker Header sticky on scroll
   window.addEventListener("scroll", function () {
-    if (window.scrollY > scrollThreshold ) {
-      // navbar.classList.add("navbar-shrink");
+    if (window.scrollY > scrollThreshold) {
       stickyNavbar.style.visibility = "visible";
       stickyNavbar.style.position = "fixed";
       stickyNavbar.style.top = "0";
-      // header.style.top = "0";
-      // headerLogo.style.display = "none";
-
-      // Create a placeholder when navigation is sticky to stop page from jumping
-      // const headerPlaceholder = document.createElement("div");
-      // headerPlaceholder.style.height = header.clientHeight + "px";
-      // header.insertBefore(headerPlaceholder, header.firstChild);
-
-      // isNavigationSticky = true;
-    }
-
-    // Reset navigation when user scrolls up to be no longer sticky
-    else if (window.scrollY < scrollThreshold ) {
-      // navbar.classList.remove("navbar-shrink");
+    } else if (window.scrollY < scrollThreshold) {
       stickyNavbar.style.visibility = "hidden";
-
-      // headerLogo.style.display = "flex";
-
-      // Remove placeholder when navigation is no longer sticky
-      // const headerPlaceholder = document.querySelector("#header-placeholder");
-      // if (headerPlaceholder) {
-      //   headerPlaceholder.remove();
-      // }
-
-      // isNavigationSticky = false;
     }
   });
 });
