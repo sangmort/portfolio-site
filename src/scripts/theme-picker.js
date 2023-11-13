6f2c73358aeee6fae7d402fb1ed87f3ec23f0298
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-13 12:30:48 -0500

    Fix ThemeIcon component so all icons update & work

--- src/scripts/theme-picker.js
+++ src/scripts/theme-picker.js
@@ -1,3 +1,4 @@
+// Script changes site from light to dark mode via button
 function setTheme(theme) {
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.setItem("theme", theme);
@@ -13,33 +14,40 @@ function initializeTheme() {
   const storedTheme = localStorage.getItem("theme");
   if (storedTheme) {
     setTheme(storedTheme);
+    updateIcons(storedTheme);
   } else {
     setTheme("light");
-  }
-  updateIcon(storedTheme);
-}
-
-function updateIcon(theme) {
-  const sun = document.querySelector(".sun");
-  const moon = document.querySelector(".moon");
-
-  if (theme === "dark") {
-    sun.classList.add("visible");
-    moon.classList.remove("visible");
-  } else {
-    sun.classList.remove("visible");
-    moon.classList.add("visible");
+    updateIcons("light");
   }
 }
 
-const toggle = document.getElementById("theme-toggle");
-toggle.addEventListener("click", () => {
-  toggleTheme();
+// Change icon appearance on click
+function updateIcons(theme) {
+  const buttons = document.querySelectorAll(".theme-toggle");
+  buttons.forEach((button) => {
+    const sun = button.querySelector(".sun");
+    const moon = button.querySelector(".moon");
 
-  // Get the updated theme after the toggle and update the icon.
-  const updatedTheme = document.documentElement.getAttribute("data-theme");
-  updateIcon(updatedTheme);
+    if (theme === "dark") {
+      sun.classList.add("visible");
+      moon.classList.remove("visible");
+    } else {
+      sun.classList.remove("visible");
+      moon.classList.add("visible");
+    }
+  });
+}
+
+// Get updated theme after toggle and update all icons
+const toggles = document.querySelectorAll(".theme-toggle");
+toggles.forEach((toggle) => {
+  toggle.addEventListener("click", () => {
+    toggleTheme();
+
+    const updatedTheme = document.documentElement.getAttribute("data-theme");
+    updateIcons(updatedTheme);
+  });
 });
 
+// Sets initial theme and icons on page load
 initializeTheme();
-
