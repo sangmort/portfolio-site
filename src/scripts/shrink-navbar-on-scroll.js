9839a45562cddff408b9b3a4987f3ad3f8e242dd
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-13 19:54:07 -0500

    Refactor sticky nav & smoothen animation

--- src/scripts/shrink-navbar-on-scroll.js
+++ src/scripts/shrink-navbar-on-scroll.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const navbar = document.querySelector(".navbar-links");
   const stickyNavbar = document.getElementById("sticky");
-  const scrollThreshold = 350;
+  const scrollThreshold = 375;
 
   window.addEventListener("scroll", function () {
     if (window.scrollY > scrollThreshold) {
-      navbar.style.visibility = "hidden";
       stickyNavbar.style.visibility = "visible";
       stickyNavbar.style.position = "fixed";
       stickyNavbar.style.top = "0";
     } else if (window.scrollY < scrollThreshold) {
       stickyNavbar.style.visibility = "hidden";
-      navbar.style.visibility = "visible";
+      stickyNavbar.style.top = "-60%";
     }
   });
 });
