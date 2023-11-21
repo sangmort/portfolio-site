5a7e03f8e04db18ada579e7ffdfe8aba5a095873
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-20 19:15:44 -0500

    Fix make first tab active styling on page load

--- src/scripts/vertical-tabs.js
+++ src/scripts/vertical-tabs.js
@@ -1,21 +1,32 @@
 // Vertical Tabs
 document.addEventListener('DOMContentLoaded', function () {
-    let tabs = document.querySelectorAll('.tab');
-    tabs.forEach(function (tab) {
-      tab.addEventListener('click', function () {
-        openTab(this.dataset.tab);
-      });
+  let tabs = document.querySelectorAll('.tab');
+  tabs.forEach(function (tab) {
+    tab.addEventListener('click', function () {
+      openTab(this.dataset.tab);
+      updateActiveTabStyle(this);
     });
-  
-    // First tab by default
-    openTab(tabs[0].dataset.tab);
   });
-  
-  function openTab(tabName) {
-    let i, tabContent;
-    tabContent = document.getElementsByClassName("tab-content");
-    for (i = 0; i < tabContent.length; i++) {
-      tabContent[i].style.display = "none";
-    }
-    document.getElementById(tabName).style.display = "block";
+
+  // First tab clicked by default
+  tabs[0].click();
+});
+
+function openTab(tabName) {
+  let i, tabContent;
+  tabContent = document.getElementsByClassName("tab-content");
+  for (i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = "none";
   }
+  document.getElementById(tabName).style.display = "block";
+}
+
+// Style the tab if they are active
+function updateActiveTabStyle(clickedTab) {
+  let tabs = document.querySelectorAll('.tab');
+  tabs.forEach(function (tab) {
+    tab.classList.remove('active-tab');
+  });
+
+  clickedTab.classList.add('active-tab');
+}
