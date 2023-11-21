363841e737bca30837988758b6190059b1d48334
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-21 11:56:45 -0500

    Refactor to detect user's system theme preference

--- src/scripts/theme-picker.js
+++ src/scripts/theme-picker.js
@@ -1,34 +1,43 @@
-// Script changes site from light to dark mode via button
+// Script changes site from light to dark mode via button &/or
+// user-preference at the system level
+
+// Sets the theme in document's attribute & local storage to remember user's preference
 function setTheme(theme) {
   document.documentElement.setAttribute("data-theme", theme);
   localStorage.setItem("theme", theme);
 }
 
+// Switch between light & dark theme based on current theme
 function toggleTheme() {
   const currentTheme = document.documentElement.getAttribute("data-theme");
   const targetTheme = currentTheme === "light" ? "dark" : "light";
   setTheme(targetTheme);
 }
 
+// Initialize theme based on user's system preference or preference in local storage
 function initializeTheme() {
+  const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const storedTheme = localStorage.getItem("theme");
+
   if (storedTheme) {
     setTheme(storedTheme);
-    updateIcons(storedTheme);
   } else {
-    setTheme("light");
-    updateIcons("light");
+    setTheme(prefersDarkMode ? "dark" : "light");
   }
+
+  updateIcons();
 }
 
-// Change icon appearance on click
-function updateIcons(theme) {
+// Updates sun & moon icons based on current theme
+function updateIcons() {
   const buttons = document.querySelectorAll(".theme-toggle");
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+
   buttons.forEach((button) => {
     const sun = button.querySelector(".sun");
     const moon = button.querySelector(".moon");
 
-    if (theme === "dark") {
+    if (currentTheme === "dark") {
       sun.classList.add("visible");
       moon.classList.remove("visible");
     } else {
@@ -38,16 +47,13 @@ function updateIcons(theme) {
   });
 }
 
-// Get updated theme after toggle and update all icons
+// Toggle theme & update icons when theme toggle buttons are clicked
 const toggles = document.querySelectorAll(".theme-toggle");
 toggles.forEach((toggle) => {
   toggle.addEventListener("click", () => {
     toggleTheme();
-
-    const updatedTheme = document.documentElement.getAttribute("data-theme");
-    updateIcons(updatedTheme);
+    updateIcons();
   });
 });
 
-// Sets initial theme and icons on page load
 initializeTheme();
