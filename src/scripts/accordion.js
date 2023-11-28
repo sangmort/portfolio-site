34baf1bc5329b1307839819ab0905c359925385b
sangmort <22422853+sangmort@users.noreply.github.com>
2023-11-27 19:43:49 -0500

    Fix accordion shifting other content

--- src/scripts/accordion.js
+++ src/scripts/accordion.js
@@ -1,27 +1,30 @@
-// A11y friendly collapsable/accordion, triggers based off accordion ID 
-document.querySelectorAll('.accordion').forEach((accordion) => {
-    const accordionButton = accordion.querySelector('.accordion-button');
-    const accordionContent = accordion.querySelector(`#${accordionButton.getAttribute('aria-controls')}`);
-  
-    // Checks accordions expanded or closed initial state and adds CSS rotation class
-    const isExpanded = accordionButton.getAttribute('aria-expanded') === 'true';
-    accordionButton.classList.toggle('rotate-icon', isExpanded);
-  
-    accordionButton.addEventListener('click', toggleAccordion);
-    accordionButton.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter' || event.key === ' ') {
-        toggleAccordion(event);
-      }
-    });
-  
-    function toggleAccordion() {
-      const isExpanded = accordionButton.getAttribute('aria-expanded') === 'true';
-      accordionContent.style.display = isExpanded ? 'none' : 'block';
-      accordionButton.setAttribute('aria-expanded', !isExpanded);
-  
-      // Toggle the rotation class
-      accordionButton.classList.toggle('rotate-icon', !isExpanded);
+// A11y friendly collapsable/accordion, triggers based off accordion ID
+document.querySelectorAll(".accordion").forEach((accordion) => {
+  const accordionButton = accordion.querySelector(".accordion-button");
+  const accordionContent = accordion.querySelector(`#${accordionButton.getAttribute("aria-controls")}`);
+
+  // Check if accordion is initially expanded and apply rotation class
+  const isExpanded = accordionButton.getAttribute("aria-expanded") === "true";
+  accordionButton.classList.toggle("rotate-icon", isExpanded);
+
+  // Click and keyboard event listeners to accordion button
+  accordionButton.addEventListener("click", toggleAccordion);
+  accordionButton.addEventListener("keydown", (event) => {
+    // Toggle accordion on 'Enter' or 'Space' key press
+    if (event.key === "Enter" || event.key === " ") {
+      toggleAccordion(event);
     }
   });
-  
-  
+
+  // Toggle accordion visibility and rotation class
+  function toggleAccordion() {
+    const isExpanded = accordionButton.getAttribute("aria-expanded") === "true";
+
+    // Toggle visibility AND display to prevent other elements from shifting when accordion expands
+    accordionContent.style.visibility = isExpanded ? "hidden" : "visible";
+    accordionContent.style.display = isExpanded ? "none" : "block";
+    accordionButton.setAttribute("aria-expanded", !isExpanded);
+
+    accordionButton.classList.toggle("rotate-icon", !isExpanded);
+  }
+});
